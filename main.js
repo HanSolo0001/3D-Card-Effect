89f829197130e713961afc480523e355be221f71
Daniel Hildreth <pillarite@gmail.com>
2023-02-15 06:16:11 -0500

    Fixed mouse not following and inverted direction

--- main.js
+++ main.js
@@ -8,5 +8,17 @@ function rotateElement(event, element) {
     // get mouse position
     const x = event.clientX;
     const y = event.clientY;
-    console.log(x, y);
+    // console.log(x, y);
+
+    // find the middle of the page for the pre element
+    const middleX = window.innerWidth / 2;
+    const middleY = window.innerHeight / 2;
+
+    // get offset from middle (how far away is mouse from middle)
+    const offsetX = ((x - middleX) / middleX) * 45;
+    const offsetY = ((y - middleY) / middleY) * 45;
+    console.log(offsetX, offsetY);
+
+    element.style.setProperty("--rotateX", -1 * offsetY + "deg");
+    element.style.setProperty("--rotateY", offsetX + "deg");
 }
